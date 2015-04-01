94483ad499d9cfa4b15a94f743e2a694250cdc66
Marcin Kondej <markondej@gmail.com>
2015-04-01 14:48:43 +0200

    major fixes

---
+++ wave_reader.h
@@ -0,0 +1,21 @@
+#ifndef WAVE_READER_H
+#define WAVE_READER_H
+
+#include <string>
+#include <vector>
+#include "pcm_wave_header.h"
+
+class WaveReader
+{
+    public:
+        WaveReader(std::string filename);
+        virtual ~WaveReader();
+        int checkDataFormat(PCMWaveHeader *header);
+        std::vector<unsigned int> *generateFreqDivs(double frequency);
+        PCMWaveHeader *getHeader();
+    private:
+        PCMWaveHeader header;
+        std::vector<char> data;
+};
+
+#endif // WAVE_READER_H
