040dd1b9663d167715cac24ae8434afbe83b45dd
Saumyakanta Sahoo <somu1795@gmail.com>
2016-05-04 06:57:14 +0530

    Updated PiStation.py

--- PiStation.py
+++ PiStation.py
@@ -6,26 +6,27 @@ parser = argparse.ArgumentParser(prog='python PiStation.py', description='Broadc
 parser.add_argument("song_file")
 parser.add_argument("-f", "--frequency", help="Set TX frequency. Acceptable range 87.1-108.2", type=float)
 arg = parser.parse_args()
-DEFAULT = object()
-param = arg.frequency
-
-def freq(param=DEFAULT):
-    if param is DEFAULT:
-	arg.frequency = '103.3'
 
 def main():
     os.system('clear')
-    freq()	
-    print ("Welcome to ImPiFM!  \nVersion 1.0 \nGPLv3 License\n")
-    print arg.song_file
-    print arg.frequency	
-    if 87.1 >= arg.frequency >= 108.2:
+    frequency = 0
+    #frequency=freq()	
+    print ("Welcome to PiStation!  \nVersion 1.0 \nGPLv3 License\n")    
+    #This block is for setting default values for frequency in case argument is not provided
+    if arg.frequency is None:
+        frequency = raw_input("Enter the frequency: ")
+        if frequency == "":
+            frequency = '103.3'
+    elif 87.1 >= arg.frequency >= 108.2:
         print "Frequency argument out of range.";exit()
+    else:
+	frequency = str(arg.frequency)
+    print frequency
     try:
         if ".mp3" in arg.song_file.lower():
-            os.system("ffmpeg -i "+arg.song_file+" "+"-f s16le -ar 22.05k -ac 1 - | sudo ./fm_transmitter -f"+" "+arg.frequency+" "+" - ")
+            os.system("ffmpeg -i "+arg.song_file+" "+"-f s16le -ar 22.05k -ac 1 - | sudo ./fm_transmitter -f"+" "+frequency+" "+" - ")
         elif ".wav" in arg.song_file.lower():	    
-	    os.system("sudo ./fm_transmitter -f"+" "+arg.frequency+" "+arg.song_file)
+	    os.system("sudo ./fm_transmitter -f"+" "+frequency+" "+arg.song_file)
         else:
             print "That file extension is not supported."
             print "File name provided: %s" %arg.song_file
