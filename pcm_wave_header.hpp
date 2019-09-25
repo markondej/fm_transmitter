b1780b5379d9724ab5145111b164d9c76a96a8be
Marcin Kondej <markondej@gmail.com>
2019-09-25 02:45:49 +0200

    Fixed WAVE header elements type definitions

--- pcm_wave_header.hpp
+++ pcm_wave_header.hpp
@@ -40,10 +40,10 @@
 
 struct PCMWaveHeader
 {
-    int8_t chunkID[4];
+    uint8_t chunkID[4];
     uint32_t chunkSize;
-    int8_t format[4];
-    int8_t subchunk1ID[4];
+    uint8_t format[4];
+    uint8_t subchunk1ID[4];
     uint32_t subchunk1Size;
     uint16_t audioFormat;
     uint16_t channels;
@@ -51,7 +51,7 @@ struct PCMWaveHeader
     uint32_t byteRate;
     uint16_t blockAlign;
     uint16_t bitsPerSample;
-    int8_t subchunk2ID[4];
+    uint8_t subchunk2ID[4];
     uint32_t subchunk2Size;
 };
 
