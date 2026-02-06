3ec5166726ab5e733a4f68e14324386840271af0
Marcin Kondej <markondej@gmail.com>
2026-02-06 23:31:56 +0000

    Improved sample conversion performance

--- wave_reader.cpp
+++ wave_reader.cpp
@@ -43,21 +43,18 @@
 Sample::Sample(uint8_t *data, unsigned channels, unsigned bitsPerChannel)
     : value(0.f)
 {
-    int sum = 0;
-    int16_t *channelValues = new int16_t[channels];
+    uint32_t sum = 0;
     for (unsigned i = 0; i < channels; i++) {
         switch (bitsPerChannel >> 3) {
         case 2:
-            channelValues[i] = (data[((i + 1) << 1) - 1] << 8) | data[((i + 1) << 1) - 2];
+            sum += *reinterpret_cast<int16_t *>(&data[i << 1]);
             break;
         case 1:
-            channelValues[i] = (static_cast<int16_t>(data[i]) - 0x80) << 8;
+            sum += (static_cast<int16_t>(data[i]) - 0x80) << 8;
             break;
         }
-        sum += channelValues[i];
     }
-    value = 2 * sum / (static_cast<float>(USHRT_MAX) * channels);
-    delete[] channelValues;
+    value = sum / (-static_cast<float>(SHRT_MIN) * channels);
 }
 
 float Sample::GetMonoValue() const
